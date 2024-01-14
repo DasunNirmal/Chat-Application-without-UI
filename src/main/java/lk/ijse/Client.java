3d17bc49e7c2571a9965699904cacd2786f9bc81
DasunNirmal <dasun200301@gmail.com>
2024-01-14 17:34:53 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/Client.java
@@ -0,0 +1,40 @@
+package lk.ijse;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+
+        try {
+            /*Create a Socket*/
+            Socket socket = new Socket("localhost",3001);
+            System.out.println("Service Started");
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));;
+
+            String message = "";
+            String reply = "";
+
+            while (!message.equals("END")) {
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+
+                message = dataInputStream.readUTF();
+                System.out.println("Server :" + message);
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/lk/ijse/Server.java
@@ -0,0 +1,45 @@
+package lk.ijse;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+
+        try {
+            /*Create server socket to listing request*/
+            ServerSocket serverSocket = new ServerSocket(3001);
+            System.out.println("Service Started");
+
+            /*Local Socket*/
+            Socket socket = serverSocket.accept();
+
+            System.out.println("Accepted");
+
+            /*To read the data*/
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "";
+            String reply = "";
+
+            while (!message.equals("END")) {
+                message = dataInputStream.readUTF();
+                System.out.println("Client :" + message);
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
